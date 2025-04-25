7a95cf899e7e19a90c8624caf87115fe6692ebda
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-25 18:07:15 +0545

    chore: added implementation

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,32 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+    let controller = window?.rootViewController as! FlutterViewController
+    let api = PigeonApiImplementation()
+    ExampleHostApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: api)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+// #docregion swift-class
+private class PigeonApiImplementation: ExampleHostApi {
+  func getHostLanguage() throws -> String {
+    return "Swift"
+  }
+
+  func add(_ a: Int64, to b: Int64) throws -> Int64 {
+    if a < 0 || b < 0 {
+      throw PigeonError(code: "code", message: "message", details: "details")
+    }
+    return a + b
+  }
+
+  func sendMessage(message: MessageData, completion: @escaping (Result<Bool, Error>) -> Void) {
+    if message.code == Code.one {
+      completion(.failure(PigeonError(code: "code", message: "message", details: "details")))
+      return
+    }
+    completion(.success(true))
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:demo_pigeon/src/messages.g.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,14 +33,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final ExampleHostApi api = ExampleHostApi();
+  int a = 0;
+
+  /// Calls host method `add` with provided arguments.
+  Future<int> add(int a, int b) async {
+    try {
+      return await api.add(a, b);
+    } catch (e) {
+      // handle error.
+      return 0;
+    }
+  }
+
+  /// Sends message through host api using `MessageData` class
+  /// and api `sendMessage` method.
+  Future<bool> sendMessage(String messageText) {
+    final MessageData message = MessageData(
+      code: Code.one,
+      data: <String, String>{'header': 'this is a header'},
+      description: 'uri text',
+    );
+    try {
+      return api.sendMessage(message);
+    } catch (e) {
+      // handle error.
+      return Future<bool>(() => true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () async {
+                final sum = await add(1, 2);
+                log(sum.toString());
+                setState(() {
+                  a = sum;
+                });
+              },
+              child: const Text("Add")),
+          Center(
+            child: Text('Sum: $a'),
+          ),
+        ],
       ),
     );
   }
