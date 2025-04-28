57ea108a3dc0f5f4daba1bbeca00fc3af49da7ef
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-28 09:57:58 +0545

    chore: added android implementation

--- android/app/src/main/kotlin/com/example/demo_pigeon/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/demo_pigeon/MainActivity.kt
@@ -1,5 +1,41 @@
 package com.example.demo_pigeon
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 
-class MainActivity: FlutterActivity()
+import ExampleHostApi
+import FlutterError
+import MessageData
+
+class MainActivity: FlutterActivity(){
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+
+    val api = PigeonApiImplementation()
+    ExampleHostApi.setUp(flutterEngine.dartExecutor.binaryMessenger, api)
+  }
+}
+
+// #docregion kotlin-class
+private class PigeonApiImplementation : ExampleHostApi {
+  override fun getHostLanguage(): String {
+    return "Kotlin"
+  }
+
+  override fun add(a: Long, b: Long): Long {
+    if (a < 0L || b < 0L) {
+      throw FlutterError("code", "message", "details")
+    }
+    return a + b
+  }
+
+  override fun sendMessage(message: MessageData, callback: (Result<Boolean>) -> Unit) {
+    if (message.code == Code.ONE) {
+      callback(Result.failure(FlutterError("code", "message", "details")))
+      return
+    }
+    callback(Result.success(true))
+  }
+}
+// #enddocregion kotlin-class
